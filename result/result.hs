8c412fdc8650b30feeeb852a5fcf6f6bcc9613a5
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-04-12 16:37:42 -0400

    example result program

---
+++ result/result.hs
@@ -0,0 +1,31 @@
+f :: (Int,Int,Int) -> Int
+f (x,y,z) = if x <= y 
+            then y 
+            else f (f (x-1, y, z), f (y-1, z, x), f (z-1, x, y))
+
+poss = [(x,y,z) | x <- [0..3], y <- [0..3], z <- [0..3]]
+
+printElements :: [(Int,Int,Int)] -> IO()
+printElements []     = return ()
+printElements (x:xs) = do 
+    putStrLn $ "f " ++ (show x) ++ " -> " ++ (show (f x))
+    printElements xs
+
+goOn = do
+    putStrLn "First input:" 
+    inp1 <- getLine
+    putStrLn "Second input:" 
+    inp2 <- getLine
+    putStrLn "Third input:" 
+    inp3 <- getLine
+
+    putStrLn "The result is:"
+    putStrLn $ show (f ((read inp1 :: Int), (read inp2 :: Int), (read inp3 :: Int)))
+
+    putStrLn " "
+    goOn
+
+main = do
+    putStrLn "Example results:"
+    printElements poss
+    goOn
