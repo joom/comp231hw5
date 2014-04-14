95f99399e7cdfe9ae3101221512aa7a3e2bf6415
barca <tylerhardenmd@gmail.com>
2014-04-13 20:40:29 -0400

    frustration increasing
    The primary issue to me, seems an inability to correctly load from the
    stack. Instead of loading from the stack the program simply appears to
    be reusing registers, which I believe is the root of this issue.

--- Homework5.s
+++ Homework5.s
@@ -6,6 +6,9 @@ promptFirst: .asciiz "\n Enter the first value: "
 promptSecond: .asciiz "\n Enter the second value: "
 promptThird: .asciiz "\n Enter the third value: "
 enter: .asciiz"\n"
+xyz: .asciiz"\n xyz"
+yzx: .asciiz"\n yzx"
+zxy: .asciiz"\n zxy"
 .text
 
 main:
@@ -43,6 +46,7 @@ main:
 	li $v0, 1
 	move $a0, $v0
 	syscall
+	j quit
 
 result1:
 	
@@ -52,7 +56,10 @@ result1:
 	syscall
 	j quit
 
-result:					#print the $a0 value to console
+result:					#print the $a2 value to console
+	addi $v0, $a2, 0
+
+
 	addi $v0, $a2, 0
 
 								#resetting sttack
@@ -66,11 +73,15 @@ result:					#print the $a0 value to console
 	
 	addi $sp, $sp 28			#moving stack pointer back up
 
+
+
 	jr $ra						#return
 
 recursiveSequence: 			#workhorse
 
-	addi $sp, $sp, -28			#move the stack down 16
+
+
+	addi $sp, $sp, -28			#move the stack down 28
 	sw $ra, 24($sp)				#save the return address
 	sw $a1, 20($sp)				#save first argument to stack
 	sw $a2, 16($sp)				#save second argument to stack
@@ -78,10 +89,11 @@ recursiveSequence: 			#workhorse
 	sw $s1, 8($sp)				#save first result to stack
 	sw $s2, 4($sp)				#save second result to stack
 	sw $s3, 0($sp)				#save third result to stack
-
+	
 	blt $a1, $a2, result	
     beq $a1, $a2, result		# if(a1 <= a2) jump to result
 
+	
     addi $a1, $a1, -1 			#x-1, call recursive on everything
 
 
@@ -94,29 +106,32 @@ recursiveSequence: 			#workhorse
 	 li $v0, 1
 	move $a0, $a3
 	syscall
-	 li $v0, 4
+
+	li $v0, 4
 	la $a0, enter
-	syscall
+	syscall 
 	jal recursiveSequence
 
 	add $s1, $v0, $zero 		#saving the value from the first recrursive call
+	 li $v0, 1
+	move $a0, $s1
 
 	addi $a1, $a1, 1 			#restore first value
 
 	addi $a2, $a2, -1 			#y-1
 
 								#swap values by 
-	addi $t0, $a1, 0			#t0 = a1
-	addi $t1, $a2, 0			#t1 = a2
-	addi $a1, $t1, 0			#a1 = t1
-	addi $a2, $t0, 0				#a2 = t2
+	addi $t0, $a2, 0			#t0 = a1
+	addi $t1, $a1, 0			#t1 = a2
+	addi $a1, $t0, 0			#a1 = t1
+	addi $a2, $t1, 0			#a2 = t2
 								#y-1,x,z
 
 									#swap values by 
 	addi $t0, $a3, 0			#t0 = a3
 	addi $t1, $a2, 0			#t1 = a2
-	addi $a1, $t1, 0			#a1 = t1
-	addi $a3, $t0, 0				#a3 = t2
+	addi $a3, $t1, 0			#a1 = t1
+	addi $a2, $t0, 0			#a3 = t2
 								#y-1, z, x
 
 	 li $v0, 1
@@ -128,31 +143,36 @@ recursiveSequence: 			#workhorse
 	 li $v0, 1
 	move $a0, $a3
 	syscall
-	 li $v0, 4
+
+
+	li $v0, 4
 	la $a0, enter
-	syscall
+	syscall 
+
 	jal recursiveSequence
 
 	add $s2, $v0, $zero 		#saving the value from the first recrursive call
-
+	 li $v0, 1
+	move $a0, $s2
 
 	addi $a1, $a1, 1 			#restore first value
-
-								#swap values by 
-	addi $t0, $a1, 0			#t0 = a1
-	addi $t1, $a2, 0			#t1 = a2
-	addi $a1, $t1, 0			#a1 = t1
-	addi $a2, $t0, 0				#a2 = t2
-								#z,y,x
-
-	addi $a1, $a1, -1			#setting z to z-1				
 
 									#swap values by 
 	addi $t0, $a3, 0			#t0 = a3
 	addi $t1, $a2, 0			#t1 = a2
-	addi $a1, $t1, 0			#a1 = t1
-	addi $a3, $t0, 0				#a3 = t2
-								#z-1, x, y
+	addi $a3, $t1, 0			#a1 = t1
+	addi $a2, $t0, 0			#a3 = t2
+								#y, z, x
+
+								#swap values by 
+	addi $t0, $a2, 0			#t0 = a1
+	addi $t1, $a1, 0			#t1 = a2
+	addi $a1, $t0, 0			#a1 = t1
+	addi $a2, $t1, 0			#a2 = t2
+								#z, y, x
+
+	addi $a1, $a1, -1 			#subtract one from first value
+
      li $v0, 1
 	move $a0, $a1
 	syscall
@@ -165,13 +185,18 @@ recursiveSequence: 			#workhorse
 	 li $v0, 4
 	la $a0, enter
 	syscall
+
+
 	jal recursiveSequence
+	add $s3, $v0, $zero 
+	 li $v0, 1
+	move $a0, $s3
 
 	addi $a1, $s0, 0			#set saved value of first recursive to a1
 	addi $a2, $s1, 0			#set saved value of second recursive to a2
 	addi $a3, $s2, 0			#set saved value of third recursive to a3
 
-	     li $v0, 1
+	 li $v0, 1
 	move $a0, $a1
 	syscall
 	 li $v0, 1
@@ -180,13 +205,15 @@ recursiveSequence: 			#workhorse
 	 li $v0, 1
 	move $a0, $a3
 	syscall
+	
+	
 	 li $v0, 4
 	la $a0, enter
-	syscall
+	syscall 
 	jal recursiveSequence		#call function on results of triple recursion
 
 
-								#resetting sttack
+								#resetting stack
 	lw $s3, 0($sp)				#load third result from stack
 	lw $s2, 4($sp)				#load second result from stack
 	lw $s1, 8($sp)				#load first result from stack
